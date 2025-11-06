6ad6d59ea56295c294e5a1e6cfa59eccb8dcae49
Julia <jul.zdzitowiecka@gmail.com>
2025-11-06 10:04:36 -0500

    add calculator folder

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -7,3 +7,14 @@ function calculateArea() {
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function calculateGrocery() {
+    let id='grocery';
+    let amount=0;
+    for(let i=1; i<=3; i++) {
+        let name=id+i;
+        amount += parseFloat(document.getElementById(name).value);
+    }
+    document.getElementById('totalamount').innerText = `The total amount is: ${amount}`;
+    
+}
